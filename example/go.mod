6ba75d45aea9d43229b05e14668c4e81f3b99f63
huhouhuam@outlook.com <huhouhuam@outlook.com>
2024-02-08 22:40:30 +0800

    add example

---
+++ example/go.mod
@@ -0,0 +1,7 @@
+module github.com/seacraft/log/example
+
+go 1.20
+
+require (
+    github.com/seacraft/log v1.0.0
+)
---
+++ example/simple.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/seacraft/log"
+
+func main() {
+	log.Infof("this is a test log, message: %s", "good")
+}
